43187778a0b9ce7e05e017203aafc9e591d4011d
kadenzipfel <kaden.zipfel@hotmail.com>
2022-03-18 12:41:53 -0700

    Add saleConfig

--- src/Contract.sol
+++ src/Contract.sol
@@ -4,5 +4,14 @@ pragma solidity 0.8.10;
 import "ERC721A/ERC721A.sol";
 
 contract HedsTape is ERC721A {
+  // pack into a single storage slot
+  struct SaleConfig {
+    uint64 price;
+    uint32 maxSupply;
+    uint32 startTime;
+  }
+
+  SaleConfig public saleConfig;
+
   constructor() ERC721A("hedsTAPE 3", "HT3") {}
 }
