608e0fb197a06155b7d21f3022134cead4f207b3
kadenzipfel <kaden.zipfel@hotmail.com>
2022-03-18 13:05:33 -0700

    Add baseUri and tokenUri

--- src/Contract.sol
+++ src/Contract.sol
@@ -14,7 +14,19 @@ contract HedsTape is ERC721A, Ownable {
 
   SaleConfig public saleConfig;
 
+  // TODO: pre-fill with uri
+  string private baseUri = '';
+
   constructor() ERC721A("hedsTAPE 3", "HT3") {}
+
+  function setBaseUri(string calldata _baseUri) external onlyOwner {
+    baseUri = _baseUri;
+  }
+
+  function tokenURI(uint tokenId) public view override returns (string memory) {
+    if (!_exists(tokenId)) revert URIQueryForNonexistentToken();
+    return baseUri;
+  }
 
   function setSaleConfig(uint64 _price, uint32 _maxSupply, uint32 _startTime) external onlyOwner {
     saleConfig = SaleConfig(
