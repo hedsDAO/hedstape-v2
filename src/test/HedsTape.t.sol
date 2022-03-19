a00d26d90fa95d23b7c7ffa8ec40cc781a2c1e22
kadenzipfel <kaden.zipfel@hotmail.com>
2022-03-19 13:36:47 -0700

    testUpdateStartTimeAsNotOwner()

--- src/test/HedsTape.t.sol
+++ src/test/HedsTape.t.sol
@@ -4,7 +4,13 @@ pragma solidity 0.8.10;
 import "ds-test/test.sol";
 import "../HedsTape.sol";
 
+interface CheatCodes {
+  function prank(address) external;
+  function expectRevert(bytes calldata) external;
+}
+
 contract ContractTest is DSTest {
+    CheatCodes cheats = CheatCodes(HEVM_ADDRESS);
     HedsTape hedsTape;
 
     function setUp() public {
@@ -20,5 +26,11 @@ contract ContractTest is DSTest {
         (, , uint32 newStartTime) = hedsTape.saleConfig();
 
         assertEq(newStartTime, 1647721808);
+    }
+
+    function testUpdateStartTimeAsNotOwner() public {
+        cheats.expectRevert(bytes("Ownable: caller is not the owner"));
+        cheats.prank(address(0));
+        hedsTape.updateStartTime(1647721808);
     }
 }
