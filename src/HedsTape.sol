5804f8eb8d0b367f2a06f9817129ba88c827feb3
Raiyan Mukhtar <raiyan.mook27@gmail.com>
2022-10-02 05:19:59 +0000

    Update HedsTape.sol

--- src/HedsTape.sol
+++ src/HedsTape.sol
@@ -15,88 +15,78 @@ error NoShares();
 /// @title ERC721 contract for https://heds.io/ HedsTape
 /// @author https://github.com/kadenzipfel
 contract HedsTape is ERC721K, Ownable {
-    struct SaleConfig {
-        uint64 price;
-        uint32 maxSupply;
-        uint32 startTime;
-        uint32 endTime;
-    }
+  struct SaleConfig {
+    uint64 price;
+    uint32 maxSupply;
+    uint32 startTime;
+    uint32 endTime;
+  }
 
-    /// @notice NFT sale data
-    /// @dev Sale data packed into single storage slot
-    SaleConfig public saleConfig;
+  /// @notice NFT sale data
+  /// @dev Sale data packed into single storage slot
+  SaleConfig public saleConfig;
 
-    string private baseUri =
-        "ipfs://QmWWQSX4sNW3RXZ2jTa7x1dSk4gjjxxgEr8YNHX6AazjSu";
+  string private baseUri = 'ipfs://QmWWQSX4sNW3RXZ2jTa7x1dSk4gjjxxgEr8YNHX6AazjSu';
 
-    address private zeroXSplit = 0xde4C3Ec08d11d14aa9324C9572b3035a6F2498C2;
+  address private zeroXSplit = 0xde4C3Ec08d11d14aa9324C9572b3035a6F2498C2;
 
-    constructor() ERC721K("hedsTAPE 6", "HT6") {
-        saleConfig.price = 0.1 ether;
-        saleConfig.maxSupply = 1000;
-        saleConfig.startTime = 1658577595;
-        saleConfig.endTime = 1658663995;
-    }
+  constructor() ERC721K("hedsTAPE 6", "HT6") {
+    saleConfig.price = 0.1 ether;
+    saleConfig.maxSupply = 1000;
+    saleConfig.startTime = 1658577595;
+    saleConfig.endTime = 1658663995;
+  }
 
-    /// @notice Mint a HedsTape token
-    /// @param _amount Number of tokens to mint
-    function mintHead(uint _amount) external payable {
-        SaleConfig memory config = saleConfig;
-        uint _price = uint(config.price);
-        uint _maxSupply = uint(config.maxSupply);
-        uint _startTime = uint(config.startTime);
-        uint _endTime = uint(config.endTime);
+  /// @notice Mint a HedsTape token
+  /// @param _amount Number of tokens to mint
+  function mintHead(uint _amount) external payable {
+    SaleConfig memory config = saleConfig;
+    uint _price = uint(config.price);
+    uint _maxSupply = uint(config.maxSupply);
+    uint _startTime = uint(config.startTime);
+    uint _endTime = uint(config.endTime);
 
-        if (_amount * _price != msg.value) revert InsufficientFunds();
-        if (_currentIndex + _amount > _maxSupply + 1) revert ExceedsMaxSupply();
-        if (block.timestamp < _startTime || block.timestamp > _endTime)
-            revert OutsideSalePeriod();
+    if (_amount * _price != msg.value) revert InsufficientFunds();
+    if (_currentIndex + _amount > _maxSupply + 1) revert ExceedsMaxSupply();
+    if (block.timestamp < _startTime || block.timestamp > _endTime) revert OutsideSalePeriod();
 
-        _safeMint(msg.sender, _amount);
-    }
+    _safeMint(msg.sender, _amount);
+  }
+ 
+  /// @notice Update baseUri - must be contract owner
+  function setBaseUri(string calldata _baseUri) external onlyOwner {
+    baseUri = _baseUri;
+  }
 
-    /// @notice Update baseUri - must be contract owner
-    function setBaseUri(string calldata _baseUri) external onlyOwner {
-        baseUri = _baseUri;
-    }
+  /// @notice Return tokenURI for a given token
+  /// @dev Same tokenURI returned for all tokenId's
+  function tokenURI(uint _tokenId) public view override returns (string memory) {
+    if (0 == _tokenId || _tokenId > _currentIndex - 1) revert URIQueryForNonexistentToken();
+    return baseUri;
+  }
 
-    /// @notice Return tokenURI for a given token
-    /// @dev Same tokenURI returned for all tokenId's
-    function tokenURI(uint _tokenId)
-        public
-        view
-        override
-        returns (string memory)
-    {
-        if (0 == _tokenId || _tokenId > _currentIndex - 1)
-            revert URIQueryForNonexistentToken();
-        return baseUri;
-    }
+  /// @notice Update sale start time - must be contract owner
+  function updateStartTime(uint32 _startTime) external onlyOwner {
+    saleConfig.startTime = _startTime;
+  }
 
-    /// @notice Update sale start time - must be contract owner
-    function updateStartTime(uint32 _startTime) external onlyOwner {
-        saleConfig.startTime = _startTime;
-    }
+  /// @notice Update sale end time - must be contract owner
+  function updateEndTime(uint32 _endTime) external onlyOwner {
+    saleConfig.endTime = _endTime;
+  }
 
-    /// @notice Update sale end time - must be contract owner
-    function updateEndTime(uint32 _endTime) external onlyOwner {
-        saleConfig.endTime = _endTime;
-    }
+  /// @notice Update max supply - must be contract owner
+  function updateMaxSupply(uint32 _maxSupply) external onlyOwner {
+    saleConfig.maxSupply = _maxSupply;
+  }
 
-    /// @notice Update max supply - must be contract owner
-    function updateMaxSupply(uint32 _maxSupply) external onlyOwner {
-        saleConfig.maxSupply = _maxSupply;
-    }
+  /// @notice Withdraw contract balance - must be contract owner
+  function withdraw() external onlyOwner {
+    (bool success, ) = payable(zeroXSplit).call{value: address(this).balance}("");
+    if (!success) revert FailedTransfer();
+  }
 
-    /// @notice Withdraw contract balance - must be contract owner
-    function withdraw() external onlyOwner {
-        (bool success, ) = payable(zeroXSplit).call{
-            value: address(this).balance
-        }("");
-        if (!success) revert FailedTransfer();
-    }
-
-    //// @notice update the withdrawal address zeroXSpilt
+  /// @notice update the withdrawal address zeroXSpilt
     function updateWithdrawAddress(address _zeroXSplit) external onlyOwner {
         require(_zeroXSplit != address(0), "Enter a valid address");
         zeroXSpilt = _zeroXSplit;
